1545bf969fbaa603475df7f9249bac0b623a2bcc
Ahindra D <official.ahindra@gmail.com>
2022-07-22 15:16:48 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { useRef, useReducer, useEffect } from "react";
-=======
-import { useState, useReducer, useEffect } from "react";
->>>>>>> ae38a9ae89b4a88a69585066a448c88628671ca0
 import Coins from "./components/Coins";
 import Holdings from "./components/Holdings";
 import Transactions from "./components/Transactions";
@@ -26,20 +22,16 @@ function reducer(state, action) {
         return { ...state, theme: "light" };
       }
 
-<<<<<<< HEAD
     case ACTIONS.COINS:
       state.coins = { ...action.payLoad };
       return { ...state };
 
-=======
->>>>>>> ae38a9ae89b4a88a69585066a448c88628671ca0
     default:
       return state;
   }
 }
 
 function App() {
-<<<<<<< HEAD
   let [state, dispatch] = useReducer(reducer, { theme: 'dark', coins: {}, holdings: [], transactions: [] });
 
   function displayCoins(data) {
@@ -63,28 +55,6 @@ function App() {
       clearInterval(intervalID.current);
     })
   }, [])
-=======
-  let [state, dispatch] = useReducer(reducer, {
-    theme: "dark",
-    coins: [],
-    holdings: [],
-    transactions: [],
-  });
-
-  useEffect(() => {
-    async function getData() {
-      // /https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2CEthereum%2Cdogecoin&vs_currencies=usd&include_24hr_change=true
-      let response = await fetch(
-        `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2CEthereum%2Cdogecoin&vs_currencies=usd&include_24hr_change=true`
-      );
-      let data = await response.json();
-      console.log(data);
-    }
-
-    getData();
-    console.log("rendered");
-  }, []);
->>>>>>> ae38a9ae89b4a88a69585066a448c88628671ca0
 
   return (
     <CryptoContext.Provider value={{}}>
