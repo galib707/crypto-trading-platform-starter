33851a14ec8a2faaab7b83de6665a2540c985c93
Ahindra D <official.ahindra@gmail.com>
2022-07-22 12:51:44 +0530

    style

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,31 @@ import CryptoContext from './contexts/CryptoContext'
 
 function App() {
   return (
-    <div className="container">
-      <main className="App">
-        <section className="top">
-          <h1 className="title">Earn some virtual money 💰</h1>
-          <p className="sub-title">To buy virtual food🍕</p>
-          <h2 className="wallet">🏛 Wallet : ${59.35}</h2>
-          <h3 className="value">Porfolio Value: ${40.56}</h3>
-        </section>
+    <CryptoContext.Provider value={{}}>
+      <div className="container">
+        <main className="App">
+          <section className="top">
+            <h1 className="title">Earn some virtual money 💰</h1>
+            <p className="sub-title">To buy virtual food🍕</p>
+            <h2 className="wallet">🏛 Wallet : ${59.35}</h2>
+            <h3 className="value">Porfolio Value: ${40.56}</h3>
+          </section>
 
-        <section className="mid">
-          <Coins />
-        </section>
+          <section className="mid">
+            <Coins />
+          </section>
 
-        <section className="bottom">
-          <div className="holdings-cont">
-            <Holdings />
-          </div>
-          <div className="transactions-cont">
-            <Transactions />
-          </div>
-        </section>
-      </main>
-    </div>
+          <section className="bottom">
+            <div className="holdings-cont">
+              <Holdings />
+            </div>
+            <div className="transactions-cont">
+              <Transactions />
+            </div>
+          </section>
+        </main>
+      </div>
+    </CryptoContext.Provider>
   )
 }
 
