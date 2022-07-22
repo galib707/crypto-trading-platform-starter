1a82694bf0f525b82f010c59401b8e5dbaebc83d
galib <ultansms@gmail.com>
2022-07-22 14:38:13 +0600

    first change

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Coins() {
-    return (
-        <div>Coins</div>
-    )
+  return (
+    <div className="container">
+      <div className="coin_icon">
+        <img src="" alt="" srcset="" />
+      </div>
+      <div className="coin_status">
+        <h1>{}</h1>
+        <h3></h3>
+        <p>Last 24hr:{}</p>
+      </div>
+    </div>
+  );
 }
